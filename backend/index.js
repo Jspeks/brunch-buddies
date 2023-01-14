c6dec876d1fcb91227b516fa1eb51884bf3b539a
unknown <jessespeks@hotmail.com>
2023-01-13 20:48:13 -0500

    working through basic steps, added 404 message for wrong routes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,18 @@
+// Modules and Globals
 require('dotenv').config
 const express = require('express')
 const app = express()
 
+//Express Settings
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+//Listen for Connections
+app.listen(process.env.PORT, () => {
+    console.log(`We're running on ${process.env.PORT}`)
+})
